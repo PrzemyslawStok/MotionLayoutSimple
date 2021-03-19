644594cbf805b681c172a7397b5d50f97bcf0d4e
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2021-03-19 18:53:13 +0100

    cd..

--- app/src/main/java/com/motionlayout/MainActivity.kt
+++ app/src/main/java/com/motionlayout/MainActivity.kt
@@ -3,19 +3,20 @@ package com.motionlayout
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.motionlayout.databinding.ActivityMainBinding
+import com.motionlayout.databinding.CatLayoutBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
 
-        //textViewSimple.text = "2342323234"
-        //mainLayout.setBackgroundColor(Color.CYAN)
-
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        /*val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.textViewSimple.text = "234234234234"
+        binding.textViewSimple.text = "234234234234"*/
+
+        val binding = CatLayoutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
     }
 }
