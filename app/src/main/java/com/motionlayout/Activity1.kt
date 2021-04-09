e7bb91ca2874ce4a70e7e4d35e0f0c20255a6fa7
= <=>
2021-04-09 19:20:44 +0200

    cd..

--- app/src/main/java/com/motionlayout/Activity1.kt
+++ app/src/main/java/com/motionlayout/Activity1.kt
@@ -2,10 +2,14 @@ package com.motionlayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.motionlayout.databinding.Activity1Binding
 
 class Activity1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_1)
+        //setContentView(R.layout.activity_1)
+
+        val binding = Activity1Binding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
