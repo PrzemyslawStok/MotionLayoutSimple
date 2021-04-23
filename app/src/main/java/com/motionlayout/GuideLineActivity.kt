59792c885c444e58d84466f63d7e61742dc75650
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2021-04-23 18:59:35 +0200

    cd..

---
+++ app/src/main/java/com/motionlayout/GuideLineActivity.kt
@@ -0,0 +1,11 @@
+package com.motionlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GuideLineActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_guide_line)
+    }
+}
