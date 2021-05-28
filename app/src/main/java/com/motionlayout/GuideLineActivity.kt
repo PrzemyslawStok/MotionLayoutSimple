3a014026c3419fc2e496f2c4d2555030c9f658f0
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2021-05-28 18:50:39 +0200

    cd..

--- app/src/main/java/com/motionlayout/GuideLineActivity.kt
+++ app/src/main/java/com/motionlayout/GuideLineActivity.kt
@@ -1,11 +1,19 @@
 package com.motionlayout
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.motionlayout.databinding.ActivityGuideLineBinding
 
 class GuideLineActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_guide_line)
+        //setContentView(R.layout.activity_guide_line)
+
+        val binding = ActivityGuideLineBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val number0 = binding.editTextNumber0.text.toString().toDoubleOrNull()
+
+
     }
 }
