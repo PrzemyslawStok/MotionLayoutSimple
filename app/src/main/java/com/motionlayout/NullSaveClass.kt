4cccc46773ad039c17edb3e8fd9f91d7a113363d
= <=>
2021-06-04 19:24:55 +0200

    cd ..

--- app/src/main/java/com/motionlayout/NullSaveClass.kt
+++ app/src/main/java/com/motionlayout/NullSaveClass.kt
@@ -1,17 +1,57 @@
 package com.motionlayout
 
 import android.util.Log
+import kotlin.random.Random
 
 class NullSaveClass{
     companion object {
+        val TAG = "NullSaveClass"
         fun function0() {
-            Log.i("NullSaveClass","Logowanie działa...")
+            Log.i(TAG,"Logowanie działa...")
 
             var name: String = "Przemysław"
             var surname: String? = "Stokłosa"
 
             //name = null
-            surname = null
+
+            if(true) {
+                surname = null
+            }
+
+            Log.i(TAG,"Długość imienia: ${name.length}")
+
+            Log.i(TAG, "Długość nazwiska: ${surname?.length}")
+
+            if(surname!=null){
+                Log.i(TAG, "Długość nazwiska: ${surname?.length}")
+            }else{
+                Log.i(TAG, "Długość nazwiska: nie zdefiniowano")
+            }
+
+            surname?.let{
+                Log.i(TAG, "Długość nazwiska {let}: ${it.length}")
+            }.also {
+                Log.i(TAG, "Długość nazwiska {also}: nie zdefiniowano")
+            }
+
+            val newName = createName()
+
+            if(newName!=null)
+                name = newName
+            else
+                name = "Przemysław"
+
+            name = createName()?:"Przemysław"
+
+        }
+
+        fun createName():String?{
+            val rand = Random.nextInt(0,10)
+
+            if(rand>5)
+                return "Piotr"
+            else
+                return null
         }
     }
 }
