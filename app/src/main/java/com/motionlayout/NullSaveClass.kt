1603dd59cb04c4dbed2b6848e028484d2071e1b4
= <=>
2021-06-04 19:01:19 +0200

    cd ..

--- app/src/main/java/com/motionlayout/NullSaveClass.kt
+++ app/src/main/java/com/motionlayout/NullSaveClass.kt
@@ -6,6 +6,12 @@ class NullSaveClass{
     companion object {
         fun function0() {
             Log.i("NullSaveClass","Logowanie działa...")
+
+            var name: String = "Przemysław"
+            var surname: String? = "Stokłosa"
+
+            //name = null
+            surname = null
         }
     }
 }
