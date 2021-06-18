1cbdda3e70c81180659b147abaf2226e671f2674
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2021-06-18 18:56:27 +0200

    cd..

--- app/src/main/java/com/motionlayout/GuideLineActivity.kt
+++ app/src/main/java/com/motionlayout/GuideLineActivity.kt
@@ -1,6 +1,7 @@
 package com.motionlayout
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 import com.motionlayout.databinding.ActivityGuideLineBinding
@@ -14,27 +15,51 @@ class GuideLineActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.buttonPlus.setOnClickListener {
-            val number0:Double? = binding.editTextNumber0.text.toString().toDoubleOrNull()
-            val number1:Double? = binding.editTextNumber1.text.toString().toDoubleOrNull()
+            val number0: Double? = binding.editTextNumber0.text.toString().toDoubleOrNull()
+            val number1: Double? = binding.editTextNumber1.text.toString().toDoubleOrNull()
 
-            number0?.let{
+            /*number0?.let{
                 val no = it
                 number1?.let{
                     Snackbar.make(binding.root, "${no+it}", Snackbar.LENGTH_SHORT).show()
                 }
-            }
+            }*/
 
-            val x: Double = number0?:0.0
-            val y: Double = number1?:0.0
+            val x: Double = number0 ?: 0.0
+            val y: Double = number1 ?: 0.0
 
+            Snackbar.make(binding.root, "${x + y}", Snackbar.LENGTH_SHORT).show()
+
+            /*
             if (number0 != null && number1 != null) {
                 val result = number0 + number1
                 Snackbar.make(binding.root, "${result}", Snackbar.LENGTH_SHORT).show()
-            }
+            }*/
 
         }
 
+        binding.buttonMinus.setOnClickListener {
+            val number0: Double? = binding.editTextNumber0.text.toString().toDoubleOrNull()
+            val number1: Double? = binding.editTextNumber1.text.toString().toDoubleOrNull()
 
+            val x: Double = number0 ?: 0.0
+            val y: Double = number1 ?: 0.0
+
+            Snackbar.make(binding.root, "${x - y}", Snackbar.LENGTH_SHORT).show()
+        }
+
+        binding.editTextNumber0.onFocusChangeListener =
+            View.OnFocusChangeListener { view: View, focus: Boolean ->
+                //Snackbar.make(binding.root, "Chyba działa", Snackbar.LENGTH_SHORT).show()
+                if (focus)
+                    binding.editTextNumber0.setSelection(
+                        0,
+                        binding.editTextNumber0.text.toString().length - 1
+                    )
+                //binding.editTextNumber0.setText("")
+
+
+            }
 
 
     }
